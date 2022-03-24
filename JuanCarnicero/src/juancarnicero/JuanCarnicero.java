304b934a90cafcf14c802ab9075cc95912d996f7
miguelmmm <manzano3.infor@gmail.com>
2022-03-24 13:58:49 +0100

    melocoton

--- JuanCarnicero/src/juancarnicero/JuanCarnicero.java
+++ JuanCarnicero/src/juancarnicero/JuanCarnicero.java
@@ -6,6 +6,7 @@ public class JuanCarnicero {
     public static void main(String[] args) {
         System.out.println("Hola Juan corto leña");
         System.out.println("Adios Juan");
+        System.out.println("Melocoton");
     }
     
 }
